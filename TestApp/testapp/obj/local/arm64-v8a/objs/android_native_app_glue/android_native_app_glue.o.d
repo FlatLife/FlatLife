2c70669b2b6e5885d1574c9ebbbb2c4f756b60e4
Shaye Mckay <mcksh759@student.otago.ac.nz>
2017-05-05 13:47:03 +1200

    added c++ test app

---
+++ TestApp/testapp/obj/local/arm64-v8a/objs/android_native_app_glue/android_native_app_glue.o.d
@@ -0,0 +1,5 @@
+./obj/local/arm64-v8a/objs/android_native_app_glue/android_native_app_glue.o: \
+  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.c \
+  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h
+
+C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h:
---
+++ TestApp/testapp/obj/local/mips64/objs/android_main/jni/__/src/android/android_main.o.d
@@ -0,0 +1,17 @@
+./obj/local/mips64/objs/android_main/jni/__/src/android/android_main.o: \
+  jni/../jni/../src/android/android_main.cc \
+  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdio.h \
+  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__config \
+  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue\android_native_app_glue.h \
+  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert \
+  jni/../src\main.h
+
+C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdio.h:
+
+C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__config:
+
+C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue\android_native_app_glue.h:
+
+C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert:
+
+jni/../src\main.h:
