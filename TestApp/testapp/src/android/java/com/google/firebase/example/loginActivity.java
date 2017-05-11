c81e82abffe49b65c1adbf6561511bd28993f8e9
blueshroom <gavo.t@hotmail.com>
2017-05-11 14:23:03 +1200

    Added onClick function to handle button IDs. Added button objects and native method call to C++.

--- TestApp/testapp/src/android/java/com/google/firebase/example/loginActivity.java
+++ TestApp/testapp/src/android/java/com/google/firebase/example/loginActivity.java
@@ -1,4 +1,5 @@
 package com.google.firebase.example;
+//need to insert package for c++ files, e.g. com.mycompany.mypackage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,5 +12,28 @@ public class loginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        Button mLogInButton = (Button)findViewById(R.id.btn_login);
+        mLogInButton.setOnClickListener(this);
+        Button mSignUpButton = (Button)findViewById(R.id.btn_signup);
+        mSignUpButton.setOnClickListener(this);
     }
+    
+    public static native void logInCall();
+    
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login: {
+                //handler for login button
+                logInCall();
+                break;
+            }
+            case R.id.btn_signup: {
+                //handler for signup button
+                break;
+            }
+            break;
+        }
+    }
+   
 }
