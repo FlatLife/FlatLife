ca5f1c96437f65e7f79a875de758907ab7556dcc
blueshroom <gavo.t@hotmail.com>
2017-05-12 13:09:09 +1200

    Cleaned up my errors that I made during editing NOT in Android Studio

--- TestApp/testapp/obj/local/x86/objs/android_native_app_glue/android_native_app_glue.o.d
+++ TestApp/testapp/obj/local/x86/objs/android_native_app_glue/android_native_app_glue.o.d
@@ -1,5 +1,5 @@
 ./obj/local/x86/objs/android_native_app_glue/android_native_app_glue.o: \
-  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.c \
-  C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h
+  C:/Users/Gavin/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.c \
+  C:/Users/Gavin/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h
 
-C:/Users/Shaye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h:
+C:/Users/Gavin/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h:
--- TestApp/testapp/src/android/java/com/google/firebase/example/loginActivity.java
+++ TestApp/testapp/src/android/java/com/google/firebase/example/loginActivity.java
@@ -1,39 +1,37 @@
 package com.google.firebase.example;
+
 //need to insert package for c++ files, e.g. com.mycompany.mypackage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.cpp.database.testapp.R;
 
+
 public class loginActivity extends AppCompatActivity {
+
+    public static native void logInCall();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
-        Button mLogInButton = (Button)findViewById(R.id.btn_login);
-        mLogInButton.setOnClickListener(this);
-        Button mSignUpButton = (Button)findViewById(R.id.btn_signup);
-        mSignUpButton.setOnClickListener(this);
-    }
-    
-    public static native void logInCall();
-    
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_login: {
-                //handler for login button
+
+        Button mLogInButton = (Button) findViewById(R.id.btn_login);
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 logInCall();
-                break;
             }
-            case R.id.btn_signup: {
-                //handler for signup button
-                break;
+        });
+        Button mSignUpButton = (Button) findViewById(R.id.btn_signup);
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
-            break;
-        }
+        });
     }
-   
 }
