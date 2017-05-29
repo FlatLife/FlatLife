d479efe790df68b652c05f3e4c61058eca9ec3ad
Shaye McKay <smckay@oucs1393.otago.ac.nz>
2017-05-29 19:27:49 +1200

    removed old android project.

--- TestApp/testapp/obj/local/x86_64/objs/android_native_app_glue/android_native_app_glue.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/x86_64/objs/android_native_app_glue/android_native_app_glue.o: \
-  C:/Users/Gavin/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.c \
-  C:/Users/Gavin/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h
-
-C:/Users/Gavin/AppData/Local/Android/sdk/ndk-bundle/build//../sources/android/native_app_glue/android_native_app_glue.h:
