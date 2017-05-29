cbc1137c2dcfda503ab4f588176a47c21594e85b
Shaye McKay <smckay@oucs1393.otago.ac.nz>
2017-05-29 17:22:08 +1200

    fixed label for displaying bill amount

--- v9/v9/BillsTableViewController.mm
+++ v9/v9/BillsTableViewController.mm
@@ -100,68 +100,23 @@
     
     if (cell==nil) {
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
-        
-    }
+    
     
     // Configure the cell...
     
-    Bill bill = list->billList[indexPath.row];
-    cell.textLabel.text = [NSString stringWithCString:bill.getBillName().c_str() encoding:[NSString defaultCStringEncoding]];
-    cell.detailTextLabel.text = [NSString stringWithCString:bill.getBillDate().c_str() encoding:[NSString defaultCStringEncoding]];
+        Bill bill = list->billList[indexPath.row];
+        cell.textLabel.text = [NSString stringWithCString:bill.getBillName().c_str() encoding:[NSString defaultCStringEncoding]];
+        cell.detailTextLabel.text = [NSString stringWithCString:bill.getBillDate().c_str() encoding:[NSString defaultCStringEncoding]];
     
-    UILabel *dollarlabel = [[UILabel alloc] init];
-    dollarlabel.text = [[NSString stringWithFormat:@"%@", @"$"] stringByAppendingString:[NSString stringWithCString:bill.getBillCost().c_str() encoding:[NSString defaultCStringEncoding]]];
-    dollarlabel.textColor = [UIColor redColor];
-    [dollarlabel setFrame:cell.frame];
-    dollarlabel.textAlignment = NSTextAlignmentRight;
-    [cell.contentView addSubview:dollarlabel];
-    
+        UILabel *dollarlabel = [[UILabel alloc] init];
+        dollarlabel.text =[[NSString stringWithFormat:@"$"] stringByAppendingString:[NSString stringWithCString:bill.getBillCost().c_str()      encoding:[NSString defaultCStringEncoding]]];
+        dollarlabel.textColor = [UIColor redColor];
+        [dollarlabel setFrame:cell.frame];
+        dollarlabel.textAlignment = NSTextAlignmentRight;
+        [cell.contentView addSubview:dollarlabel];
+    }
     return cell;
 }
 
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
