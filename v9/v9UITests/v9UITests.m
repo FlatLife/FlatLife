94d89a6bc8791a8573c720d943ef768d865a9cc6
Joshua Lieshout <machmatic@gmail.com>
2017-08-03 14:15:57 +1200

    Added UI tests for adding notices and chores

--- v9/v9UITests/v9UITests.m
+++ v9/v9UITests/v9UITests.m
@@ -13,6 +13,8 @@
 @end
 
 @implementation v9UITests
+    XCUIApplication *app;
+
 
 - (void)setUp {
     [super setUp];
@@ -22,7 +24,10 @@
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
+    
+    app = [[XCUIApplication alloc] init];
+    [app launch];
+    
     
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
@@ -32,7 +37,57 @@
     [super tearDown];
 }
 
-- (void)testExample {
+
+- (void)testUIAddNotice {
+    //[app.alerts[@"\U201cv9\U201d Would Like to Send You Notifications"].buttons[@"Allow"] tap];
+    [app.tabBars.buttons[@"Notices"] tap];
+    
+    NSUInteger oldCount = app.tables.cells.count;
+    
+    XCUIElement *addButton = app.navigationBars[@"Notices"].buttons[@"Add"];
+    [addButton tap];
+    
+    XCUIElement *element = [[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element;
+    XCUIElement *element2 = [[[[element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element;
+    XCUIElement *textField = [element2 childrenMatchingType:XCUIElementTypeTextField].element;
+    [textField tap];
+    
+    [app.keys[@"t"] tap];
+    [app.keys[@"e"] tap];
+    [app.keys[@"s"] tap];
+    [app.keys[@"t"] tap];
+    
+    XCUIElement *saveButton = app.navigationBars[@"Add Notice"].buttons[@"Save"];
+    [saveButton tap];
+    
+    XCTAssertEqual(app.tables.cells.count, oldCount+1);
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testUIAddChore {
+    //[app.alerts[@"\U201cv9\U201d Would Like to Send You Notifications"].buttons[@"Allow"] tap];
+    [app.tabBars.buttons[@"Chores"] tap];
+    
+    NSUInteger oldCount = app.tables.cells.count;
+    
+    XCUIElement *addButton = app.navigationBars[@"Chores List"].buttons[@"Add"];
+    [addButton tap];
+    
+    XCUIElement *element = [[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element;
+    XCUIElement *element2 = [[[[element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element;
+    XCUIElement *textField = [element2 childrenMatchingType:XCUIElementTypeTextField].element;
+    [textField tap];
+    
+    [app.keys[@"t"] tap];
+    [app.keys[@"e"] tap];
+    [app.keys[@"s"] tap];
+    [app.keys[@"t"] tap];
+    
+    XCUIElement *saveButton = app.navigationBars[@"New Chore"].buttons[@"Save"];
+    [saveButton tap];
+    
+    XCTAssertEqual(app.tables.cells.count, oldCount+1);
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
